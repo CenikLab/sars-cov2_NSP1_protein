87f0ecd42f191dd3c8dc206b940008921042d216
hakanozadam <hakan.ozadam@utexas.edu>
2021-03-04 02:00:08 -0600

    added new datasets

---
+++ nsp1_abundance/README.md
@@ -0,0 +1,1 @@
+This folder contains the comparison of our NSP1 RNA-Seq experiments to two publicly available datasets: GSE150316 and GSE159191.
---
+++ nsp1_abundance/download__srrs_GSE159191.sh
@@ -0,0 +1,64 @@
+echo GSM4822344
+fasterq-dump -p -o fastqs/GSM4822344.fastq SRR12789543
+gzip fastqs/GSM4822344*fastq
+############################
+echo GSM4822345
+fasterq-dump -p -o fastqs/GSM4822345.fastq SRR12789544
+gzip fastqs/GSM4822345*fastq
+############################
+echo GSM4822346
+fasterq-dump -p -o fastqs/GSM4822346.fastq SRR12789545
+gzip fastqs/GSM4822346*fastq
+############################
+echo GSM4822347
+fasterq-dump -p -o fastqs/GSM4822347.fastq SRR12789546
+gzip fastqs/GSM4822347*fastq
+############################
+echo GSM4822348
+fasterq-dump -p -o fastqs/GSM4822348.fastq SRR12789547
+gzip fastqs/GSM4822348*fastq
+############################
+echo GSM4822349
+fasterq-dump -p -o fastqs/GSM4822349.fastq SRR12789548
+gzip fastqs/GSM4822349*fastq
+############################
+echo GSM4822350
+fasterq-dump -p -o fastqs/GSM4822350.fastq SRR12789549
+gzip fastqs/GSM4822350*fastq
+############################
+echo GSM4822351
+fasterq-dump -p -o fastqs/GSM4822351.fastq SRR12789550
+gzip fastqs/GSM4822351*fastq
+############################
+echo GSM4822352
+fasterq-dump -p -o fastqs/GSM4822352.fastq SRR12789551
+gzip fastqs/GSM4822352*fastq
+############################
+echo GSM4822353
+fasterq-dump -p -o fastqs/GSM4822353.fastq SRR12789552
+gzip fastqs/GSM4822353*fastq
+############################
+echo GSM4822354
+fasterq-dump -p -o fastqs/GSM4822354.fastq SRR12789553
+gzip fastqs/GSM4822354*fastq
+############################
+echo GSM4822355
+fasterq-dump -p -o fastqs/GSM4822355.fastq SRR12789554
+gzip fastqs/GSM4822355*fastq
+############################
+echo GSM4822356
+fasterq-dump -p -o fastqs/GSM4822356.fastq SRR12789555
+gzip fastqs/GSM4822356*fastq
+############################
+echo GSM4822357
+fasterq-dump -p -o fastqs/GSM4822357.fastq SRR12789556
+gzip fastqs/GSM4822357*fastq
+############################
+echo GSM4822358
+fasterq-dump -p -o fastqs/GSM4822358.fastq SRR12789557
+gzip fastqs/GSM4822358*fastq
+############################
+echo GSM4822359
+fasterq-dump -p -o fastqs/GSM4822359.fastq SRR12789558
+gzip fastqs/GSM4822359*fastq
+############################
